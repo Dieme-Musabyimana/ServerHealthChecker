df02dc753abc7cd5841f564835589b144adcfcc8
Dieme-Musabyimana <musabyimanadieme10@gmail.com>
2026-06-05 18:55:31 +0200

    Feature: Send request to servers return and print status_codes and urls

--- Checker.py
+++ Checker.py
@@ -1,6 +1,10 @@
 import json
+from http.client import responses
+
+import requests
 
 
+#feature1: load servers
 def load_servers():
     with open("config.json") as file:
         data = json.load(file)
@@ -10,5 +14,18 @@ servers = load_servers()
 for server in servers:
        print(f"{server}")
 print("Loaded " + str(len(servers)) + " servers")
+
+#feature2: check server and print status codes
+def check_server(url):
+    response = requests.get(url)
+    status_code = response.status_code
+    return {
+        "url": url,
+        "status_code": status_code
+    }
+
+for server in servers:
+    results = check_server(server)
+    print(results)
 
 
