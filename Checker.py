835d4f8463f1973b2034939f420009cd19a05ba3
Dieme-Musabyimana <musabyimanadieme10@gmail.com>
2026-06-05 19:09:53 +0200

    Feature3: Implement Measuring the response time

--- Checker.py
+++ Checker.py
@@ -1,4 +1,5 @@
 import json
+import time
 from http.client import responses
 
 import requests
@@ -15,17 +16,23 @@ for server in servers:
        print(f"{server}")
 print("Loaded " + str(len(servers)) + " servers")
 
-#feature2: check server and print status codes
+#feature2: check server print status codes
 def check_server(url):
+    start_time = time.time()
     response = requests.get(url)
     status_code = response.status_code
+    end_time = time.time()
+    response_time = round((end_time-start_time)*1000,2)
     return {
         "url": url,
-        "status_code": status_code
+        "status_code": status_code,
+        "response_time": response_time
     }
 
 for server in servers:
     results = check_server(server)
     print(results)
+
+print()
 
 
